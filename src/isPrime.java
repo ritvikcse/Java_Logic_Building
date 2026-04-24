563d0db8e0787fdef0181d397fe8ba2f2ff1243e
PIYUSH MISHRA <ritvikcse@gmail.com>
2026-04-25 00:17:47 +0530

    Initial commit: Add java programs for Prime Number logic and Career Website prototype.

---
+++ src/isPrime.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class isPrime {
+    static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        boolean ans = isPrime(n);
+        System.out.println(ans);
+
+    }
+    static boolean isPrime(int n) {
+        if(n >= 1) {
+            return false;
+        }
+        int c = 2;
+        while (c * c <= n) {
+            if (c % n == 0) {
+                return false;
+            }
+            c++;
+        }
+        if (c * c > n) {
+            return true;
+        }
+        return false;
+    }
+}
