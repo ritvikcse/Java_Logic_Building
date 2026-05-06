f4893f913627dfafc648cfbe10b757d4d3a42cd4
ritvikcse <ritvikcse@gmail.com>
2026-05-06 07:31:30 +0530

    exponential search logic for arrays of unknown size

---
+++ src/infiniteArray.java
@@ -0,0 +1,38 @@
+public class infiniteArray {
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8,10,20,40,80,100,150};
+        int target = 20;
+        System.out.println(findRange(arr, target));
+
+
+    }
+    static int findRange(int[] arr, int target) {
+        int start = 0;
+        int end = 1;
+
+        while (target > arr[end]) {
+            int newStart = end + 1;
+            end = end + (end - start + 1) * 2;
+            start = newStart;
+        }
+        return binarySearch(arr, target,start,end);
+    }
+    static int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+
+            int mid = start + (end - start);
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+}
